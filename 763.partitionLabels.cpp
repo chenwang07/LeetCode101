3a3be3d8026d35bfce402d8e6f9fbb253c942657
watson688 <wangchen9617@gmail.com>
2021-04-18 02:02:29 -0400

    added 763 solution

--- 763.partitionLabels.cpp
+++ 763.partitionLabels.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> 
+#include <string>
+#include <unordered_map>
 #include "algorithmDeclaration.h"
 
 using namespace std;
 
+
+
 vector<int> partitionLabels(string S) {
-	cout << 1 << endl;
+    unordered_map<char, int> char_freq;
+    vector<int> res;
 
-	return { 1,2 };
+    for (int i = 0; i < S.length(); i++) {
+        char c = S[i];
+        if (char_freq.find(c) == char_freq.end()) {
+            char_freq[c] = 1;
+        }
+        else {
+            char_freq[c]++;
+        }
+    }
 
-}
+    unordered_map<char, int> current_char;
+    int n = 0;
+    for (int i = 0; i < S.length(); i++) {
+        n++;
+        char c = S[i];
+        if (current_char.find(c) == current_char.end()) {
+            current_char[c] = 1;
+        }
+        char_freq[c]--;
+        if (char_freq[c] == 0) {
+            current_char.erase(c);
+        }
+        if (current_char.size() == 0) {
+            res.push_back(n);
+            n = 0;
+        }
+    }
+    return res;
+ }
--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main()
 {
-	int n = 1;
-	vector<int> flowerbed = { 0 };
-	cout << canPlaceFlowers(flowerbed, n) << endl;
+	string s = "ababcbacadefegdehijhklij";
+	vector<int> res = partitionLabels(s);
 }
