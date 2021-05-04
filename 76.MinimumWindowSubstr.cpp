3744446dc8a00f4fe8ab237e2eba443e88562d0e
watson688 <wangchen9617@gmail.com>
2021-05-04 15:15:42 -0400

    updated 76

--- 76.MinimumWindowSubstr.cpp
+++ 76.MinimumWindowSubstr.cpp
@@ -7,14 +7,41 @@
 
 
 std::string minWindow(std::string s, std::string t) {
-    std::unordered_map<char, int> freq;
-    for (int i=0; i < s.size(); i++) {
-        freq[s[i]]++;
-    }
-    int i = 1;
-    for (; i < 10; i++) {
-        std::cout << i << std::endl;
-    }
-    return s;
+    int left = 0, right = 0;
+    int subStrHead = 0;
+    int minSize = s.size() + 1;
+    int count = 0;
 
+    std::unordered_map<char, int> patternCount;
+    std::unordered_map<char, int> freqCount;
+
+    for (int i = 0; i < t.size(); i++) {
+        patternCount[t[i]]++;
+        freqCount[t[i]] = 0;
+    }
+
+    for (; right < s.size(); right++) {
+        if (patternCount.find(s[right]) != patternCount.end()) {
+            if (++freqCount[s[right]] == patternCount[s[right]]) count++;
+            while (count == patternCount.size()) {
+                if (right - left + 1 < minSize) {
+                    subStrHead = left;
+                    minSize = right - left + 1;
+                }
+                if (patternCount.find(s[left]) != patternCount.end()) {
+                    freqCount[s[left]]--;
+                    if (freqCount[s[left]] < patternCount[s[left]]) count--;
+                }
+                ++left;
+            }
+        }
+    }
+    return minSize > s.size() ? "" : s.substr(subStrHead, minSize);
 }
+
+
+/*
+    string s = "ADOBECODEBANC";
+    string t = "ABC";
+    string res = minWindow(s, t);
+*/
